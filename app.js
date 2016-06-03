59d136ef38a44053b045ebbcdf06f35c29a9f764
Gabriel <gabrielpablorc@gmail.com>
2016-06-03 02:20:28 -0300

    Test server

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-var tp_arq = null;
+//	arq.js
 
-//	El Comandante was here.
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync(__dirname + '/index.html');
+
+// Send index.html to all requests
+var app = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
+});
+
+// Socket.io server listens to our app
+var io = require('socket.io').listen(app);
+
+// Send current time to all connected clients
+function sendTime() {
+    io.emit('time', { time: new Date().toJSON() });
+}
+
+// Send current time every 10 secs
+setInterval(sendTime, 5000);
+
+// Emit welcome message on connection
+io.on('connection', function(socket) {
+    // Use socket to communicate with this particular client only, sending it it's own id
+    socket.emit('welcome', { message: 'Welcome!', id: socket.id });
+
+    socket.on('i am client', console.log);
+});
+
+app.listen(3000);
